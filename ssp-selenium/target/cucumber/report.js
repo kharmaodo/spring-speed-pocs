4f838b9928f12052d5a6a51c896064c0102da8cf
azaddyne kamal <kamal.azaddyne@gmail.com>
2015-02-28 18:40:13 +0100

    removing target folder

--- ssp-selenium/target/cucumber/report.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri('org/slevin/cucumber/GoogleSearch.feature');
-formatter.feature({
-  "id": "search-on-google",
-  "description": "As an user\nI want to search on Google\nSo that I can see results",
-  "name": "Search on Google",
-  "keyword": "Feature",
-  "line": 1
-});
-});
