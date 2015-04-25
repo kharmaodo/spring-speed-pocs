e6aa108be46746bef013b7ab455298f86e81fd7e
azaddyne kamal <kamal.azaddyne@gmail.com>
2015-04-26 01:54:27 +0200

    trying to add list of accounts on ssp-mongodb

--- ssp-batch-mongodb/src/main/java/org/slevin/batch/model/Report.java
+++ ssp-batch-mongodb/src/main/java/org/slevin/batch/model/Report.java
@@ -1,66 +1,78 @@
 package org.slevin.batch.model;
 
+import org.slevin.common.Account;
+
 import javax.xml.bind.annotation.XmlRootElement;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 
 @XmlRootElement(name = "report-mongo")
 public class Report {
 
-	private int id;
-	private Date date;
-	private long impression;
-	private int clicks;
-	private BigDecimal earning;
+    private int id;
+    private Date date;
+    private long impression;
+    private int clicks;
+    private BigDecimal earning;
+    private List<Account> accounts;
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-	public long getImpression() {
-		return impression;
-	}
+    public long getImpression() {
+        return impression;
+    }
 
-	public void setImpression(long impression) {
-		this.impression = impression;
-	}
+    public void setImpression(long impression) {
+        this.impression = impression;
+    }
 
-	public int getClicks() {
-		return clicks;
-	}
+    public int getClicks() {
+        return clicks;
+    }
 
-	public void setClicks(int clicks) {
-		this.clicks = clicks;
-	}
+    public void setClicks(int clicks) {
+        this.clicks = clicks;
+    }
 
-	public BigDecimal getEarning() {
-		return earning;
-	}
+    public BigDecimal getEarning() {
+        return earning;
+    }
 
-	public void setEarning(BigDecimal earning) {
-		this.earning = earning;
-	}
+    public void setEarning(BigDecimal earning) {
+        this.earning = earning;
+    }
 
-	@Override
-	public String toString() {
-		SimpleDateFormat dateFormatter = new SimpleDateFormat("M/d/yyyy");
+    public List<Account> getAccounts() {
+        return accounts;
+    }
 
-		return "Report [id=" + id + ", date=" + dateFormatter.format(date) + ", impression=" + impression + ", clicks=" + clicks + ", earning="
-				+ earning + "]";
-	}
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("M/d/yyyy");
+
+        return "Report [id=" + id + ", date=" + dateFormatter.format(date) + ", impression=" + impression + ", clicks=" + clicks + ", earning="
+                + earning + "]";
+    }
 
 }
