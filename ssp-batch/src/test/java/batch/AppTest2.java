1ca03de7f76e7e27d99d3dd4720f88fc92f92180
azaddyne kamal <kamal.azaddyne@gmail.com>
2015-04-25 16:59:02 +0200

    Test for ssp-batch xml to csv

---
+++ ssp-batch/src/test/java/batch/AppTest2.java
@@ -0,0 +1,29 @@
+package batch;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(locations = {
+        "classpath:META-INF/spring/jobs/job-accounts.xml",
+        "classpath:META-INF/spring/config/applicationContext.xml",
+        "classpath:META-INF/spring/config/database.xml",
+        "classpath:spring/batch/config/test-context.xml"})
+public class AppTest2 extends AbstractTestNGSpringContextTests {
+
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+
+	@Test
+	public void launchJob() throws Exception {
+
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+		Assert.assertEquals(jobExecution.getStatus(), BatchStatus.COMPLETED);
+
+	}
+}
